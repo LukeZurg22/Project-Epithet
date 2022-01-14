1ad487991f6c2f882773f06687fba61e2e4d5908
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2022-01-14 01:02:08 -0600

    Add project files.

---
+++ Control_Stats.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Epithet_Erased_System
+{
+    /// <summary>
+    /// Interaction logic for Stat.xaml
+    /// </summary>
+    public partial class Stat : UserControl
+    {
+        public static DependencyProperty StrokeProperty = DependencyProperty.Register("Stroke", typeof(Brush), typeof(Stat));
+        public Brush Stroke
+        {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static new DependencyProperty FontFamilyProperty = DependencyProperty.Register("FontFamily", typeof(FontFamily), typeof(Stat));
+        public new FontFamily FontFamily
+        {
+            get { return (FontFamily)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(Stat));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static DependencyProperty TitleColor = DependencyProperty.Register("Foreground", typeof(Brush), typeof(Stat));
+        public new Brush Foreground
+        {
+            get { return (Brush)GetValue(TitleColor); }
+            set { SetValue(TitleColor, value); }
+        }
+
+        public Stat()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+    }
+}
---
+++ Page_Character.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Epithet_Erased_System
+{
+
+    public partial class CharacterSheet : UserControl
+    {
+        public static new DependencyProperty BackgroundProperty = DependencyProperty.Register("Background", typeof(Brush), typeof(CharacterSheet));
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public static DependencyProperty PageBackgroundProperty = DependencyProperty.Register("PageBackground", typeof(Brush), typeof(CharacterSheet));
+        public Brush PageBackground
+        {
+            get { return (Brush)GetValue(PageBackgroundProperty); }
+            set { SetValue(PageBackgroundProperty, value); }
+        }
+
+        public CharacterSheet()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Name_Margin(object sender, TextChangedEventArgs e)
+        {
+            /*            //if (Box_Name.Text.Length > 0)
+                        {
+                            Box_Name.Width = 12 + (Box_Name.Text.Length * 6);
+                        }
+            */
+        }
+
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            var brush = new SolidColorBrush((Color)ColorPicker.SelectedColor);
+            Background = brush;
+        }
+    }
+}
